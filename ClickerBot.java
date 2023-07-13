61089a6eab651cbd5a440f843552819582908688
pmaj <majpawel90@gmail.com>
2023-07-13 21:46:56 +0200

    fix

--- ClickerBot.java
+++ ClickerBot.java
@@ -1,9 +1,7 @@
 import java.awt.*;
 import java.awt.event.InputEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
-public class ClickerBot implements MouseListener {
+public class ClickerBot {
 	private Robot robot;
 
 	private Utils utils;
@@ -62,31 +60,6 @@ public class ClickerBot implements MouseListener {
 		System.out.println("Position X: " + x);
 		System.out.println("Position Y: " + y);
 		System.out.println("{"+x+","+y+"}");
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		System.out.println("Click");
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-
 	}
 }
 
