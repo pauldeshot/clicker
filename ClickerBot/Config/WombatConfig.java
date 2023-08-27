6d906fbb27e8170f9d3a034836ccd98254cea291
pmaj <majpawel90@gmail.com>
2023-08-27 11:31:29 +0200

    feat: wombat config in json

--- ClickerBot/Config/WombatConfig.java
+++ ClickerBot/Config/WombatConfig.java
@@ -1,5 +1,12 @@
 package ClickerBot.Config;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class WombatConfig {
     // Konfiguracja dotycząca czasu czekania
     public int waitRun = 5 * 60 + 1;
@@ -21,5 +28,33 @@ public class WombatConfig {
     }
 
     public WombatConfig() {
+
+        String path = "wombat.json";
+        String content = null;
+        try {
+            content = new String(Files.readAllBytes(Paths.get(path)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        JSONObject jsonObject = new JSONObject(content);
+
+        waitRun = (int) jsonObject.get("waitRun");
+        maxRuns = (int) jsonObject.get("maxRuns");
+        runButton = getCoordinate(jsonObject, "runButton");
+        rewardButton = getCoordinate(jsonObject, "rewardButton");
+        wombatTab = getCoordinate(jsonObject, "wombatTab");
+        refresh = getCoordinate(jsonObject, "refresh");
+        treasure = getCoordinate(jsonObject, "treasure");
+        requestAll = getCoordinate(jsonObject, "requestAll");
+        helpAll = getCoordinate(jsonObject, "helpAll");
+    }
+
+    public int[] getCoordinate(JSONObject arr, String element) {
+        JSONArray sunflower = arr.getJSONArray(element);
+        int[] coordinate = new int[sunflower.length()];
+        for (int i = 0; i < sunflower.length(); i++) {
+            coordinate[i] = sunflower.getInt(i);
+        }
+        return coordinate;
     }
 }
